f31ef1c8adfee2b09c7a732c0700d5683fac7d88
tommitah <reijotahvanainen15@gmail.com>
2021-01-26 14:04:48 +0200

    eslint double quotation rule

--- src/App.js
+++ src/App.js
@@ -1,42 +1,38 @@
-import React, { useEffect, useState } from 'react'
-import { Switch, Route, Redirect, useLocation } from 'react-router-dom'
-import { useSelector, useDispatch } from 'react-redux'
+import React, { useEffect, useState } from "react"
+import { Switch, Route, Redirect, useLocation } from "react-router-dom"
+import { useSelector, useDispatch } from "react-redux"
 
-import LandingPage from './components/LandingPage'
-import HomePage from './components/HomePage'
-import ProfilePage from './components/ProfilePage'
-import WorkersPage from './components/WorkersPage'
-import ContractsPage from './components/ContractsPage'
-import PrivateRoute from './components/PrivateRoute'
-import ProcessPage from './components/ProcessPage'
-import TasksPage from './components/TaskPage'
-import MessagePage from './components/MessagePage'
-import DocumentPage from './components/DocumentPage'
-import Drawer from './components/Drawer'
-import AppBar from './components/AppBar'
+import LandingPage from "./components/LandingPage"
+import HomePage from "./components/HomePage"
+import ProfilePage from "./components/ProfilePage"
+import WorkersPage from "./components/WorkersPage"
+import ContractsPage from "./components/ContractsPage"
+import PrivateRoute from "./components/PrivateRoute"
+import ProcessPage from "./components/ProcessPage"
+import TasksPage from "./components/TaskPage"
+import MessagePage from "./components/MessagePage"
+import DocumentPage from "./components/DocumentPage"
+import Drawer from "./components/Drawer"
+import AppBar from "./components/AppBar"
 
-import { clearAlert } from './actions/alertActions'
-import Role from './utils/role'
+import { clearAlert } from "./actions/alertActions"
+import Role from "./utils/role"
 
-import {
-  CssBaseline,
-  Snackbar,
-  Toolbar
-} from '@material-ui/core'
-import { Alert } from '@material-ui/lab'
+import { CssBaseline, Snackbar, Toolbar } from "@material-ui/core"
+import { Alert } from "@material-ui/lab"
 
 /**
  * App component, main react component which acts as a container for all the other components.
  * @exports App
  */
 const App = () => {
-  const { loggedIn, data } = useSelector(state => state.user)
-  const alert = useSelector(state => state.alert)
+  const { loggedIn, data } = useSelector((state) => state.user)
+  const alert = useSelector((state) => state.alert)
   const dispatch = useDispatch()
   const [open, setOpen] = useState(false)
 
   const handleSnackbarClose = (_event, reason) => {
-    if (reason !== 'clickaway') {
+    if (reason !== "clickaway") {
       dispatch(clearAlert())
     }
   }
@@ -48,7 +44,7 @@ const App = () => {
    * @function
    */
   const handleDrawer = () => {
-    setOpen(prevOpen => !prevOpen)
+    setOpen((prevOpen) => !prevOpen)
   }
 
   // extra toolbar prevents content from going underneath appbar.
@@ -58,9 +54,18 @@ const App = () => {
       <CssBaseline />
       <AppBar handleDrawer={handleDrawer} />
       <Toolbar />
-      <Drawer open={open} handleDrawer={handleDrawer} loggedIn={loggedIn} role={data.role} />
+      <Drawer
+        open={open}
+        handleDrawer={handleDrawer}
+        loggedIn={loggedIn}
+        role={data.role}
+      />
       <Snackbar open={alert.open} onClose={handleSnackbarClose}>
-        <Alert onClose={handleSnackbarClose} severity={alert.severity} variant="filled">
+        <Alert
+          onClose={handleSnackbarClose}
+          severity={alert.severity}
+          variant="filled"
+        >
           {alert.message}
         </Alert>
       </Snackbar>
@@ -78,42 +83,48 @@ const App = () => {
           path="/contracts"
           role={data.role}
           roles={[Role.Agency]}
-          loggedIn={loggedIn}>
+          loggedIn={loggedIn}
+        >
           <ContractsPage />
         </PrivateRoute>
         <PrivateRoute
           path="/workers"
           role={data.role}
           roles={[Role.Business, Role.Agency]}
-          loggedIn={loggedIn}>
+          loggedIn={loggedIn}
+        >
           <WorkersPage />
         </PrivateRoute>
         <PrivateRoute
           path="/process"
           role={data.role}
           roles={[Role.Worker]}
-          loggedIn={loggedIn}>
+          loggedIn={loggedIn}
+        >
           <ProcessPage />
         </PrivateRoute>
         <PrivateRoute
           path="/tasks"
           role={data.role}
           roles={[Role.Worker, Role.Agency, Role.Business]}
-          loggedIn={loggedIn}>
+          loggedIn={loggedIn}
+        >
           <TasksPage />
         </PrivateRoute>
         <PrivateRoute
           path="/messages"
           role={data.role}
           roles={[Role.Worker, Role.Agency, Role.Business]}
-          loggedIn={loggedIn}>
+          loggedIn={loggedIn}
+        >
           <MessagePage />
         </PrivateRoute>
         <PrivateRoute
           path="/documents"
           role={data.role}
           roles={[Role.Worker, Role.Agency, Role.Business]}
-          loggedIn={loggedIn}>
+          loggedIn={loggedIn}
+        >
           <DocumentPage />
         </PrivateRoute>
         <Redirect from="*" to="/home" />
--- src/components/DocumentPage/index.js
+++ src/components/DocumentPage/index.js
@@ -1,13 +1,8 @@
-import React from 'react'
+import React from "react"
 
-
-import {
-  Container,
-  Typography
-} from '@material-ui/core'
+import { Container, Typography } from "@material-ui/core"
 
 const DocumentHome = () => {
-
   return (
     <Container maxWidth="md" disableGutters>
       <Typography>Document</Typography>
